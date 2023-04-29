1d1a15cf0101e54e9945beedef33b870819cc9db
Misha <kompreso@vk.com>
2023-04-29 11:10:07 +0300

    Fixed bug with zero rights check, added abstract class SRE_SetRights

--- library/simple-rights-engine.php
+++ library/simple-rights-engine.php
@@ -7,7 +7,7 @@
         }
         public static function CheckOneOfRights($userrights, $requiredrights)
         {
-            return (($userrights & $requiredrights) != 0);
+            return ($requiredrights == 0 || ($userrights & $requiredrights) != 0);
         }
     }
     abstract class SRE_Rights
@@ -18,6 +18,16 @@
         const gen_moder    = 0b00001000;
         const admin        = 0b00010000;
         const gen_admin    = 0b00100000;
+        const contentmaker = 0b01000000;
+    }
+    abstract class SRE_SetRights
+    {
+        const tester       = 0b00000001;
+        const helper       = 0b00000010;
+        const moder        = 0b00000110;
+        const gen_moder    = 0b00001110;
+        const admin        = 0b00011110;
+        const gen_admin    = 0b00111110;
         const contentmaker = 0b01000000;
     }
 ?>
